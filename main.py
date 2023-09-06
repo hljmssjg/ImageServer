f2971b494eb26b19c64c08696747df3766ffc1b4
Jiangeng Sun <843722226@qq.com>
2023-09-06 19:02:01 +0200

    upload a small project

---
+++ main.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template, request, send_file
+import os
+
+app = Flask(__name__)
+
+# 设置上传文件夹路径和下载文件夹路径
+UPLOAD_FOLDER = 'upload'
+DOWNLOAD_FOLDER = 'download'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['DOWNLOAD_FOLDER'] = DOWNLOAD_FOLDER
+
+@app.route('/')
+def welcome():
+    return render_template('welcome.html')
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return "No file part"
+
+        file = request.files['file']
+
+        if file.filename == '':
+            return "No selected file"
+
+        if file:
+            filename = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+            file.save(filename)
+            return "File uploaded successfully"
+    return render_template('upload.html')
+
+@app.route('/download')
+def download_page():
+    files = os.listdir(app.config['DOWNLOAD_FOLDER'])
+    return render_template('download.html', files=files)
+
+@app.route('/download/<filename>')
+def download(filename):
+    filepath = os.path.join(app.config['DOWNLOAD_FOLDER'], filename)
+    return send_file(filepath, as_attachment=True)
+
+if __name__ == '__main__':
+    app.run(debug=True)
